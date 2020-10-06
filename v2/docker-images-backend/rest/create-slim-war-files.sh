1d5922e99a6d451f8ec61bb51edb7cffe4f0a9cb
Oliver Heger <oliver.heger@bosch.io>
2020-10-06 13:36:22 +0200

    chore(v2): add an image for the REST API
    This image contains a Tomcat with a deployment of the components
    implementing the SW360 REST API.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ v2/docker-images-backend/rest/create-slim-war-files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright Bosch.IO GmbH 2020.
+# Part of the SW360 Portal Project.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+
+set -e
+
+rm -rf libs
+mkdir libs
+rm -rf slim-wars
+mkdir slim-wars
+for i in $(ls *.war)
+do
+  i=${i%.war}
+  echo repacking $i ...
+  rm -rf $i 
+  mkdir $i 
+  cd $i && unzip -q ../$i.war
+  mv WEB-INF/lib/* ../libs/
+  rmdir WEB-INF/lib
+  zip -q -r ../slim-wars/$i.war .
+  cd ..
+  echo done
+done
