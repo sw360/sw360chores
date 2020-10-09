a13fed3523cfdce3659568194577299221eaa639
Oliver Heger <oliver.heger@bosch.io>
2020-10-09 09:16:11 +0200

    chore(v2): provide CouchDB credentials as secrets
    User name and password for CouchDB are no longer passed as hard-coded
    environment variables in docker-compose.yaml. They are now defined as
    secrets. The images have been adapted to correctly extract the
    credentials from the corresponding secret files.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

--- v2/docker-images-backend/couchdb-lucene/docker-entrypoint.sh
+++ v2/docker-images-backend/couchdb-lucene/docker-entrypoint.sh
@@ -10,6 +10,15 @@
 
 set -e
 
+COUCHDB_USER_FILE=/run/secrets/COUCHDB_USER
+if [ -f "$COUCHDB_USER_FILE" ]; then
+    export COUCHDB_USER=$(cat "$COUCHDB_USER_FILE")
+fi
+COUCHDB_PASSWORD_FILE=/run/secrets/COUCHDB_PASSWORD
+if [ -f "$COUCHDB_PASSWORD_FILE" ]; then
+    export COUCHDB_PASSWORD=$(cat "$COUCHDB_PASSWORD_FILE")
+fi
+
 if [ ! "$COUCHDB_HOST" ]; then
     echo "the environmental variable \$COUCHDB_HOST must be set"
     exit 1
--- v2/docker-images-backend/rest/entrypoint.sh
+++ v2/docker-images-backend/rest/entrypoint.sh
@@ -7,6 +7,15 @@
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 
+COUCHDB_USER_FILE=/run/secrets/COUCHDB_USER
+if [ -f "$COUCHDB_USER_FILE" ]; then
+    export COUCHDB_USER=$(cat "$COUCHDB_USER_FILE")
+fi
+COUCHDB_PASSWORD_FILE=/run/secrets/COUCHDB_PASSWORD
+if [ -f "$COUCHDB_PASSWORD_FILE" ]; then
+    export COUCHDB_PASSWORD=$(cat "$COUCHDB_PASSWORD_FILE")
+fi
+
 envsubst < /etc/sw360/couchdb.properties.template > /etc/sw360/couchdb.properties
 envsubst < /etc/sw360/sw360.properties.template > /etc/sw360/sw360.properties
 
